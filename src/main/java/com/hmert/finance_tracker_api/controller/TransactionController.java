702b58cfd21cfbfd713c31a79fdfdaabec12a317
Hasan Yilmaz <mailin@gmail.com>
2026-04-28 16:02:29 +0200

    feat: add summary endpoint for income and expense

--- src/main/java/com/hmert/finance_tracker_api/controller/TransactionController.java
+++ src/main/java/com/hmert/finance_tracker_api/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.hmert.finance_tracker_api.controller;
 import com.hmert.finance_tracker_api.entity.Transaction;
 import com.hmert.finance_tracker_api.service.TransactionService;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 import java.util.List;
 
@@ -25,4 +26,13 @@ public class TransactionController {
     public List<Transaction> findAll() {
         return service.findAll();
     }
+
+    @GetMapping("/balance")
+    public Double getBalance() {
+        return service.getBalance();
+}
+    @GetMapping("/summary")
+    public Map<String, Double> getSummary() {
+        return service.getSummary();
+}
 }
--- src/main/java/com/hmert/finance_tracker_api/service/TransactionService.java
+++ src/main/java/com/hmert/finance_tracker_api/service/TransactionService.java
@@ -3,6 +3,8 @@ package com.hmert.finance_tracker_api.service;
 import com.hmert.finance_tracker_api.entity.Transaction;
 import com.hmert.finance_tracker_api.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
 import java.util.List;
 
@@ -24,4 +26,42 @@ public class TransactionService {
     public List<Transaction> findAll() {
         return repository.findAll();
     }
+
+    // BALANCE
+public Double getBalance() {
+    List<Transaction> transactions = repository.findAll();
+
+    double balance = 0;
+
+    for (Transaction t : transactions) {
+        if ("INCOME".equalsIgnoreCase(t.getType())) {
+            balance += t.getAmount();
+        } else if ("EXPENSE".equalsIgnoreCase(t.getType())) {
+            balance -= t.getAmount();
+        }
+    }
+
+    return balance;
+}
+
+public Map<String, Double> getSummary() {
+    List<Transaction> transactions = repository.findAll();
+
+    double income = 0;
+    double expense = 0;
+
+    for (Transaction t : transactions) {
+        if ("INCOME".equalsIgnoreCase(t.getType())) {
+            income += t.getAmount();
+        } else if ("EXPENSE".equalsIgnoreCase(t.getType())) {
+            expense += t.getAmount();
+        }
+    }
+
+    Map<String, Double> summary = new HashMap<>();
+    summary.put("income", income);
+    summary.put("expense", expense);
+
+    return summary;
+}
 }
